73e01b51d7b87dfa0bdd1549cb61e6c2b086d290
Sak <sarkisssbarsegian@gmail.com>
2025-08-11 00:23:21 +0400

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-echo -n "Enter your name: "
-read Name
-echo "hello $Name"
+echo -n "Enter your age: "
+read age
+if [[ $age -gt 0 ]] && [[ $age -le 12 ]]; then
+        echo "You are to young"
+    elif [[ $age -gt 12 ]] && [[ $age -le 18 ]]; then
+        echo "You are a teenager"
+    elif [[ $age -gt 19 ]] && [[ $age -le 40 ]]; then
+        echo "You are adult"
+    else
+        echo "You are old asf"
+fi
+
